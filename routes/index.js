e0b74ab179056343f68bfdb6acd6dc19fa80e31c
BstDragonLay <efrainlaycastillo@hotmail.com>
2017-08-13 19:08:52 -0500

    cambios leves

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'RevendeloPanama' });
+});
+
+/* GET Acces page */
+router.get('/cuenta', function(req, res, next) {
+  res.render('register', { title: 'Registrate' });
 });
 
 module.exports = router;
